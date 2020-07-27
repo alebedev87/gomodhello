aebbe4d7102ad340f0593e4d539e6fcebffcad52
Andrey LEBEDEV <andrey.lebedev@amadeus.com>
2020-07-27 15:00:50 +0200

    initial commit

---
+++ hello.go
@@ -0,0 +1,13 @@
+package gomodhello
+
+import "rsc.io/quote/v3"
+
+// Hello ...
+func Hello() string {
+    return quote.HelloV3()
+}
+
+// Proverb ...
+func Proverb() string {
+    return quote.Concurrency()
+}
